c10d68b7b029043e300436968e3471457a71511c
AhmedMegahedTolba <ahmed.megahed.tolba@gmail.com>
2023-10-25 17:44:39 +0300

    calc commit

---
+++ CFG/KEYBAD_Cfg.c
@@ -0,0 +1,11 @@
+#include "stdtypes.h"
+#include "Utils.h"
+#include "KEYBAD_Cfg.h"
+
+ u8 KEYBAD_Arr[ROWS][COLS]={
+	 {'7','8','9','/'},
+	 {'4','5','6','*'},
+	 {'1','2','3','-'},
+	 {'C','0','=','+'},
+	  
+ };
